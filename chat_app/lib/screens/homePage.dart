ae50475f88cd382ac762614a11056db5459060f0
realtanx <hamniba8@gmail.com>
2021-02-19 22:29:26 +0800

    [Chat App] add search bar and bottom tab bar

---
+++ chat_app/lib/screens/homePage.dart
@@ -0,0 +1,25 @@
+import 'package:chat_app/screens/chatPage.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Chat"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.group_work), label: "Channel"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_box), label: "Profile")
+        ],
+      ),
+    );
+  }
+}
